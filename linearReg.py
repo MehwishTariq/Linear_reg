c7cf5ed0acb56f2ebbd4a6563c87e07f6459a04f
Mehwish Tariq <34317601+MehwishTariq@users.noreply.github.com>
2020-01-25 15:44:28 +0500

    Add files via upload

---
+++ linearReg.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 27 00:10:02 2019
+
+@author: Mehwish Tariq Ameen
+"""
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+class Linear_Reg:
+    def __init__(self,path,x=0,y=0,theta=0):
+        self.x=np.array(x)
+        self.y=np.array(y)
+        self.theta=np.array(theta)        
+        self.data = pd.read_csv(path)
+    
+    def plot(self,x,y):
+        plt.scatter(x,y)
+        plt.xlabel('Money spent on TV ads ($)')
+        plt.ylabel('Sales ($)')
+        self.draw_line()
+        plt.show()
+                
+    def define_x(self):
+        X= np.array(self.data)
+        self.x = X[:,1] 
+        #self.m=np.shape(self.x)[0]
+        return self.x
+    
+    def reshape_x(self):
+        return self.x.reshape(-1,1)
+    
+    def reshape_y(self):
+        return self.y.reshape(-1,1)
+    
+    def define_y(self):
+        Y = np.array(self.data)
+        self.y = Y[:,-1] 
+        return self.y
+    
+    def draw_line(self):
+        reg = LinearRegression()
+        reg.fit(self.reshape_x(),self.reshape_y())
+        predictions = reg.predict(self.reshape_x())
+        plt.plot(self.x,predictions,c='black',linewidth=3)
+        plt.show()
+        
+        
+line = Linear_Reg("data\Advertising.csv")
+line.plot(line.define_x(),line.define_y())
